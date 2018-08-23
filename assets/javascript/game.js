1d9dffb51e3420c805d2d83a5c6106b454093844
theinfamousAK <akhilkalepu@protonmail.com>
2018-08-23 16:43:52 -0400

    basic html, css andjs variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// ----------------VARIABLES----------------
+// CHARACTER VARIABLES
+var characters = ["luke", "rey", "kylo", "snoke"];
+var userCharacter = [];
+var enemyCharacters = [];
+var defender = [];
+var defeatedCharacters = [];
+
+// HEALTH VARIABLES
+var lukeHealth = 120;
+var reyHealth = 100;
+var kyloHealth = 150;
+var snokeHealth = 180;
+
+// ATTACK VARIABLES
+var attackPower = 8;
+var counterPower = 25;
+
+// ----------------GAME----------------
+// CHOOSE CHARACTER
+$("#luke".click(function()) {
+    console.log("luke was clicked");
+});

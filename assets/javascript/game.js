d250b3607c5561c27dbf6919b1cae97558b3025f
theinfamousAK <akhilkalepu@protonmail.com>
2018-08-23 21:31:09 -0400

    health point variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
+
 // ----------------VARIABLES----------------
 // CHARACTER VARIABLES
 var characters = ["luke", "rey", "kylo", "snoke"];
@@ -12,12 +14,36 @@ var reyHealth = 100;
 var kyloHealth = 150;
 var snokeHealth = 180;
 
+    // PUSH HEALTH POINTS TO CARDS
+    var lukePointsHtml = 
+    "<h4 class='card-title'>Luke Skywalker</h4>" +
+    "<p class='card-text'>Health Points: " + lukeHealth + "</p>";
+    $("#luke-points").html(lukePointsHtml);
+
+    var reyPointsHtml = 
+    "<h4 class='card-title'>Rey</h4>" +
+    "<p class='card-text'>Health Points: " + lukeHealth + "</p>";
+    $("#rey-points").html(reyPointsHtml);
+
+    var kyloPointsHtml = 
+    "<h4 class='card-title'>Kylo Ren</h4>" +
+    "<p class='card-text'>Health Points: " + lukeHealth + "</p>";
+    $("#kylo-points").html(kyloPointsHtml);
+
+    var snokePointsHtml = 
+    "<h4 class='card-title'>Snoke</h4>" +
+    "<p class='card-text'>Health Points: " + lukeHealth + "</p>";
+    $("#snoke-points").html(snokePointsHtml);
+    
 // ATTACK VARIABLES
 var attackPower = 8;
 var counterPower = 25;
 
 // ----------------GAME----------------
 // CHOOSE CHARACTER
-$("#luke".click(function()) {
-    console.log("luke was clicked");
+// $("#luke").on("click", function() {
+//     console.log("luke clicked");
+// });
+
+// ------------------------------------------------
 });
